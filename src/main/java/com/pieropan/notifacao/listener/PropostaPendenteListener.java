0e5c017b5197fbe3ae9f961c73453b4c74108602
AlanLR <alanlopes.r@gmail.com>
2025-06-05 10:42:12 -0300

    FEAT: criando configuração para escutar a fila com RabbitListener

--- src/main/java/com/pieropan/notifacao/listener/PropostaPendenteListener.java
+++ src/main/java/com/pieropan/notifacao/listener/PropostaPendenteListener.java
@@ -1,13 +1,32 @@
 package com.pieropan.notifacao.listener;
 
+import com.pieropan.notifacao.constante.MensagemConstante;
+import com.pieropan.notifacao.domain.Proposta;
+import com.pieropan.notifacao.service.NotificacaoService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class PropostaPendenteListener {
 
-    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
-    public void propostaPendente(){
+    private NotificacaoService notificacaoService;
 
+    public PropostaPendenteListener(NotificacaoService notificacaoService){
+        this.notificacaoService = notificacaoService;
     }
+
+    //Essa anotação indica que esse método vai ser um consumidor de mensagens da fila informada
+    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+    public void propostaPendente(Proposta proposta){
+        if (Objects.isNull(proposta) || Objects.isNull(proposta.getUsuario()) || Objects.isNull(proposta.getUsuario().getNome())) {
+            System.err.println("Proposta ou usuário nulo: " + proposta);
+            return;
+        }
+
+        String mensagem = String.format(MensagemConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
+        notificacaoService.notificar(mensagem);
+    }
+
 }
--- src/main/java/com/pieropan/notifacao/service/NotificacaoService.java
+++ src/main/java/com/pieropan/notifacao/service/NotificacaoService.java
@@ -1,4 +1,12 @@
 package com.pieropan.notifacao.service;
 
+import com.pieropan.notifacao.domain.Proposta;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificacaoService {
+
+    public void notificar(String mensagem){
+
+    }
 }
